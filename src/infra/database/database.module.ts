7fcef3afe8add4978913a1fe19d5d3fff15ee6f2
Thales <thales0591@gmail.com>
2025-05-12 21:50:09 -0300

    feat: implement abstract class and use injectable decorator at  domain use case

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -6,17 +6,26 @@ import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prism
 import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-respository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 
 @Module({
   providers: [
     PrismaService,
-    PrismaQuestionsRepository,
+    { provide: QuestionsRepository, useClass: PrismaQuestionsRepository },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
     PrismaAnswerAttachmentsRepository,
     PrismaAnswerCommentsRepository,
   ],
-  exports: [PrismaService],
+  exports: [
+    PrismaService,
+    QuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+  ],
 })
 export class DatabaseModule {}

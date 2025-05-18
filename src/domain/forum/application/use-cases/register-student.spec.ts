13805c62404806aba9e6762431b03f8cb1b4199b
Thales <thales0591@gmail.com>
2025-05-18 19:08:30 -0300

    test: register student unit test

---
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -0,0 +1,42 @@
+import { RegisterStudentCase } from './register-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let sut: RegisterStudentCase
+
+describe('Register Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+
+    sut = new RegisterStudentCase(inMemoryStudentsRepository, fakeHasher)
+  })
+
+  it('should be able to register a new student', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      student: inMemoryStudentsRepository.items[0],
+    })
+  })
+
+  it('should hash student password upon registration', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    })
+
+    const hashedPassword = await fakeHasher.hash('123456')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryStudentsRepository.items[0].password).toEqual(hashedPassword)
+  })
+})
